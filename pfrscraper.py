d890b78f6408574b19c1f3ae0a2712a5fffaf69a
Rob <robert.ryan.harrison@gmail.com>
2020-08-25 21:38:57 -0400

    updated with sam's version

--- pfrscraper.py
+++ pfrscraper.py
@@ -1,59 +1,39 @@
-#Rob's Original Version - Conversion To Float/Int Doesn't work on 'None'
+#Sam's Revised Version
 
 
 
 
-from bs4 import BeautifulSoup as Soup
+# import needed packages
+from bs4 import BeautifulSoup as bs
 import requests
-from pandas import DataFrame
-from os import path
-#import packages needed
+import pandas as pd
 
 
-pfr_response = requests.get('https://www.pro-football-reference.com/years/2019/passing.htm')
-pfr_soup = Soup(pfr_response.text,"html.parser")
 # pfr_soup is a nested tag, so call find_all on it
+pfr_response = requests.get('https://www.pro-football-reference.com/years/2019/passing.htm')
+soup = bs(pfr_response.text,"html.parser")
 
-tables = pfr_soup.find_all('table')
-#create a list of tables in the html
+# create empty lists for later use
+headers = []
+data_by_row = []
 
-#print(len(tables))
-#unhash this len above to ensure there is only 1 table in html being pulled
+# identify all table, rows, and headers on first page
+table = soup.find('table')
+table_rows = table.find_all('tr')
+table_headers = table.find_all('th')
 
-pfr_table = tables[0]
-#if there is more than 1 table this needs to be adjusted
+# extract headers
+for th in table_headers:
+    headers.append(th.text.replace('\n', ' ').strip())
 
-rows = pfr_table.find_all('tr')
-#list of all the tr tags inside our table.
+for tr in table_rows:
+        td = tr.find_all('td')
+        row = [i.text.replace('\n', ' ').strip() for i in td]
+        data_by_row.append(row)
 
-#x = rows[1]
-#print(x)
+# there were multiple headers in the table so I limited the list two the first row of headers
+headers = headers[1:31]
 
-
-def parse_row(row):
-#function to take in a tr tag and get the data out of it in the form of a list of strings.
-    return [str(x.string) for x in row.find_all('td')]
-
-list_of_parsed_rows = [parse_row(row) for row in rows[1:]]
-df = DataFrame(list_of_parsed_rows)
-#create pandas dataframe with all rows of data in it (starting at row 1 excludes header)
-
-
-df.columns = ['Player Name', 'Team', 'Age', 'Position', 'Games Played', 'Games Started', 'QBStartRecord', 'Completions', 'Attempts', 'Completion %', 'Passing Yards', 'Passing TDs', 'TD %', 'Interceptions', 'Int %', '1D', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'QBR', 'Sacked', 'SackYds', 'NY/A', 'ANY/A', 'Sack%', '4QComebacks', 'GWD']
-#manually enter headers (easiest way)
-
-#float_cols =['Completion %', 'Int %', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'QBR', 'NY/A', 'ANY/A', 'Sack%']
-#int_cols =['Age', 'Games Played', 'Games Started', 'Completions', 'Attempts', 'Passing Yards', 'Passing TDs', 'Interceptions', '1D', 'Lng', 'Sacked', 'SackYds', '4QComebacks', 'GWD']
-#df[float_cols] = df[float_cols].astype(float)
-#df[int_cols] = df[int_cols].astype(int)
-#structure non-string data
-
-#df.drop('columnname', axis=1, inplace=True)
-#use this funtion to drop columns if i don't want them
-
-
-
-
-
+df = pd.DataFrame(data_by_row, columns= headers)
 
 df.to_csv('2019Passing.csv', index=False)
